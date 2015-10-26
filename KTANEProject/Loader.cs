0faf01766def4605f0307ed25b65f3974eb245df
Justin Mazzola <jmazzola@fullsail.edu>
2015-10-25 23:08:19 -0400

    Added project :cat2:

---
+++ KTANEProject/Loader.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+namespace KTANEProject
+{
+    public class Loader
+    {
+        public static GameObject gameObject;
+
+        public static void Load()
+        {
+            // Create a new gameobject
+            gameObject = new GameObject();
+            // Add our script to it
+            gameObject.AddComponent<Menu>();
+            // Never delete this until we close the game
+            UnityEngine.Object.DontDestroyOnLoad(gameObject);
+        }
+    }
+}
---
+++ KTANEProject/Menu.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+using System.Runtime.InteropServices;
+
+namespace KTANEProject
+{
+    public class Menu : MonoBehaviour
+    {
+        
+        public string customSeed = "Custom seed";
+
+        void Start()
+        {
+             
+        }
+
+        void Update()
+        {
+
+            ProcessInput();
+        }
+
+        void ProcessInput()
+        {
+
+        }
+
+        void OnGUI()
+        {
+            GUI.color = Color.magenta;
+
+                             //x  y
+            GUI.Label(new Rect(0, 0, 300, 40), "KTANEProject v1.0 - By Justin 'Chocolate' Mazzola");
+
+            // Set Seed: [textbox]
+            GUI.Label(new Rect(0, 50, 100, 40), "Set Seed: ");
+            customSeed = GUI.TextField(new Rect(110, 50, 100, 20), customSeed);
+
+        }
+    }
+}
